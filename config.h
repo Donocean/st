4c99a78441cd2cfe527e4a4ab2a288e68945b9ce
Donocean <1184427366@qq.com>
2022-08-20 15:45:40 +0800

    little change

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 
-static char *font = "Consolas:pixelsize=24:antialias=true:autohint=true";
+static char *font = "Sauce Code Pro NF:pixelsize=24:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
